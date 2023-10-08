6683454d1c246879e145d96f375e9390710c3dab
yujiromorota07 <yujiromorota07@gmail.com>
2023-10-08 14:59:14 +0900

    cloud runにデプロイできるようにする

--- main.tf
+++ main.tf
@@ -3,3 +3,21 @@ provider "google" {
   project     = var.project_id 
   region      = var.default_region
 }
+
+resource "google_cloud_run_service" "gcp-tutorial-server" {
+  name     = var.project_id
+  location = var.default_region
+
+  template {
+    spec {
+      containers {
+        image = "asia.gcr.io/${var.project_id}/gcp-tutorial-server"
+      }
+    }
+  }
+
+  traffic {
+    percent         = 100
+    latest_revision = true
+  }
+}
